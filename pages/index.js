12507b5bf42f1cdf1eb8ad6552cd640686c4ea53
Save <save9253@mail.ru>
2020-11-16 16:13:20 -0500

    width with no scroll fixed

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import { useState, useEffect } from 'react';
+
+// Width with no scroll bar
+function winWidth() {
+  const [VW, setVW] = useState(undefined)
+
+  useEffect(() => {
+    function handleResize() {
+      setVW(document.documentElement.clientWidth + "px");
+    }
+
+    window.addEventListener("resize", ()=>{
+      handleResize()
+    });
+    
+    handleResize();
+    
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); 
+
+  return VW;
+}
 
 export default function Home() {
+  const size = winWidth();
+  if(size != undefined){document.documentElement.style.setProperty('--vw',size)}
   return (
-    <main>
+    <main id="content">
       <Head>
         <title>How to build a website?</title>
         <link rel="icon" href="/SavesDesignFavicon.png" />
@@ -206,41 +230,43 @@ export default function Home() {
         </section>
       </section>
       <section aria-label="Choice" className={styles.end}>
-        <h2>It is your choice in the end</h2>
-        <div className={styles.leftArr}>
-            <svg className={styles.arrowB} viewBox="0 0 40 30" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0 0H40V20L45 17.5V27.5L20 40L-5 27.5V17.5L0 20Z"/>
-            </svg>
-        </div>
-        <Link href="/login"><a className={styles.left} id="no"><span>Learn how to make websites <br/><b>on your own!</b></span></a></Link>
-        <div className={styles.rightArr}>
-            <svg className={styles.arrowB} viewBox="0 0 40 30" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0 0H40V20L45 17.5V27.5L20 40L-5 27.5V17.5L0 20Z"/>
-            </svg>
-        </div>
-        <Link href="/shop"><a className={styles.right} id="yes"><span>Make your website <br/><b>with Save!</b></span></a></Link>
-        <svg role="img" aria-label="Save" className={styles.face} viewBox="0 0 149 203" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g fill="var(--md50)">
-                <path id="LeyeSh" d="M47.8 91.5L61.7 99.7L66.1 105.2L72.6 111.1L73.3 103.5L71.5 93.3L65.2 91.7L47.8 91.5Z"/>
-                <path id="Rshadow" d="M121.8 51.8L122.8 77L119.6 86.6L117.8 93.7L119.3 103.5L125.9 117.1L126.3 134.7L117.6 159.2L108.9 180.1L109.1 187.2L120.7 171.8L129.3 154.1L135.4 153.5L140.7 144L147.5 124L143.9 109.4L138.4 109.2L132.7 116.9L129.9 88.5L131.2 80.9L131.2 65.9L121.8 51.8Z"/>
-                <path id="Lshadow" d="M26.9 69.4L28.1 85.7L23.6 100.8L25.6 129.1L32.6 163.4L48.6 181.5L51 189.9L30.2 167.5L22.9 151.7L18.7 114.6L17.1 100.2L23 86.4L21.3 71.4L31.9 54.2L26.9 69.4Z"/>
-                <path id="nose" d="M88.6 97.3L98.2 96L115.9 95.3L94.5 100.3L89.4 107.7L90 122.3L96.5 129L93.9 138.8L81.8 140.4L75.9 140.4L62.9 137.8L60.4 128.7L63.8 134.5L73.1 132.3L80.6 134.7L87.3 132.3L89.4 126.8L85.2 119.5L85.1 106.7L88.6 97.3Z"/>
-                <path id="TLip" d="M66 159.7L67.2 157.4L76.7 152.4L79.2 152.7L82.1 151.4L96.7 151.7L98.1 154.2L95.9 153L83.5 154L80.1 154.9L76.8 155.2L68.7 158.3L66 159.7Z"/>
-                <path id="BLip" d="M72.6 165.1L77.7 170.2L87.8 169L92.2 162.5L88.5 160.7L77.7 162.5L72.6 165.1Z"/>
-            </g>
-            <g fill="var(--dr)">
-                <path id="beard" d="M48.6 181.5L51 189.9L68.4 201.2L91.1 199.9L109.1 187.2L108.9 180.1L90 188.2L65.6 189.2L48.6 181.5Z"/>
-                <path id="hair" d="M132.7 116.9L129.9 88.5L131.2 80.9L131.2 65.9L119.1 47.6L115.8 31.5L105.9 17.3L112.6 31.4L115.3 46.8L105.7 44.5L98.1 25.2L76.1 13.2L94.3 25.4L102.2 45.3L96.2 44.7L82.1 32.7L54.9 31.9L79.8 34.9L92.1 46L79.7 47.4L65.9 52.8L55.1 60.4L31.9 54.2L21.3 71.4L23 86.4L17.1 100.2L18.7 114.6L9.1 107.3L7.4 61.8L10.2 41.2L14.7 38.1L14.6 31.3L26.2 16.8L41.7 8.2L46.9 8.6L50 4L68.8 1.2L89.2 3.5L94 7.5L96.2 5.6L117.2 12.3L131 28.8L133.4 36.3L136 39.8L141.1 58L142.1 87L138.4 109.2L132.7 116.9Z"/>
-                <path id="Lnosed" d="M66.1 135.8L71.1 135.4L74.6 136.9L69 137.4L66.1 135.8Z"/>
-                <path id="Rnosed" d="M83.8 137.7L87.6 135.7L92.3 136.6L88 138.3L83.8 137.7Z"/>
-                <path id="Reye" d="M104.6 97.5L112.5 98.3L118.6 102.4L111.9 100.6L108.8 100.4L108 103.5L104.4 105L100.6 103.4L100 100.4L95.4 101.3L92.8 102.9L94.5 100.3L104.6 97.5Z"/>
-                <path id="Leye" d="M54.1 97.3L61.7 99.7L63.9 102.4L61.1 100.6L58.4 100.3L57.9 102.9L53.8 104.4L49.4 102.8L48.9 99.9L45.6 99.8L40.8 101.6L45.2 98.6L54.1 97.3Z"/>
-                <path id="mustch" d="M57.4 156.6L58.9 151.2L73.7 146.2L79.5 146.2L83.8 144.7L103.5 146.1L104 151.5L101 149.1L84.9 147.8L79.6 149.5L74.3 150L60.3 154.6L57.4 156.6Z"/>
-                <path id="ReyeB" d="M88.6 97.3L92.4 90.3L115.7 90.7L122 96.1L115.9 95.3L98.2 95.9L88.6 97.3Z"/>
-                <path id="LeyeB" d="M35 93.4L46.3 87L67 84.4L71.5 93.3L65.2 91.7L47.8 91.5L35 93.4Z"/>
-            </g>
-            <path id="outline" d="M9.1 107.3L7.4 61.8L10.2 41.2L14.7 38.1L14.6 31.3L26.2 16.8L41.7 8.2L46.9 8.6L50 4L68.8 1.2L89.2 3.5L94 7.5L96.2 5.6L117.2 12.3L131 28.8L133.4 36.3L136 39.8L141.1 58L142.1 87L138.4 109.2L143.9 109.4L147.5 124L140.7 144L135.4 153.5L129.3 154.1L120.7 171.8L109.1 187.2L91.1 199.9L68.4 201.2L51 189.9L30.2 167.5L22.9 151.7L16.9 152.1L8.4 144.3L1.5 124.3L4.1 109.8L9.1 107.3Z" stroke="black" strokeOpacity="0.9" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round"/>
-        </svg>
+        <section>
+          <h2>It is your choice in the end</h2>
+          <div className={styles.leftArr}>
+              <svg className={styles.arrowB} viewBox="0 0 40 30" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M0 0H40V20L45 17.5V27.5L20 40L-5 27.5V17.5L0 20Z"/>
+              </svg>
+          </div>
+          <Link href="/login"><a className={styles.left} id="no"><span>Learn how to make websites <br/><b>on your own!</b></span></a></Link>
+          <div className={styles.rightArr}>
+              <svg className={styles.arrowB} viewBox="0 0 40 30" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M0 0H40V20L45 17.5V27.5L20 40L-5 27.5V17.5L0 20Z"/>
+              </svg>
+          </div>
+          <Link href="/shop"><a className={styles.right} id="yes"><span>Make your website <br/><b>with Save!</b></span></a></Link>
+          <svg role="img" aria-label="Save" className={styles.face} viewBox="0 0 149 203" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g fill="var(--md50)">
+                  <path id="LeyeSh" d="M47.8 91.5L61.7 99.7L66.1 105.2L72.6 111.1L73.3 103.5L71.5 93.3L65.2 91.7L47.8 91.5Z"/>
+                  <path id="Rshadow" d="M121.8 51.8L122.8 77L119.6 86.6L117.8 93.7L119.3 103.5L125.9 117.1L126.3 134.7L117.6 159.2L108.9 180.1L109.1 187.2L120.7 171.8L129.3 154.1L135.4 153.5L140.7 144L147.5 124L143.9 109.4L138.4 109.2L132.7 116.9L129.9 88.5L131.2 80.9L131.2 65.9L121.8 51.8Z"/>
+                  <path id="Lshadow" d="M26.9 69.4L28.1 85.7L23.6 100.8L25.6 129.1L32.6 163.4L48.6 181.5L51 189.9L30.2 167.5L22.9 151.7L18.7 114.6L17.1 100.2L23 86.4L21.3 71.4L31.9 54.2L26.9 69.4Z"/>
+                  <path id="nose" d="M88.6 97.3L98.2 96L115.9 95.3L94.5 100.3L89.4 107.7L90 122.3L96.5 129L93.9 138.8L81.8 140.4L75.9 140.4L62.9 137.8L60.4 128.7L63.8 134.5L73.1 132.3L80.6 134.7L87.3 132.3L89.4 126.8L85.2 119.5L85.1 106.7L88.6 97.3Z"/>
+                  <path id="TLip" d="M66 159.7L67.2 157.4L76.7 152.4L79.2 152.7L82.1 151.4L96.7 151.7L98.1 154.2L95.9 153L83.5 154L80.1 154.9L76.8 155.2L68.7 158.3L66 159.7Z"/>
+                  <path id="BLip" d="M72.6 165.1L77.7 170.2L87.8 169L92.2 162.5L88.5 160.7L77.7 162.5L72.6 165.1Z"/>
+              </g>
+              <g fill="var(--dr)">
+                  <path id="beard" d="M48.6 181.5L51 189.9L68.4 201.2L91.1 199.9L109.1 187.2L108.9 180.1L90 188.2L65.6 189.2L48.6 181.5Z"/>
+                  <path id="hair" d="M132.7 116.9L129.9 88.5L131.2 80.9L131.2 65.9L119.1 47.6L115.8 31.5L105.9 17.3L112.6 31.4L115.3 46.8L105.7 44.5L98.1 25.2L76.1 13.2L94.3 25.4L102.2 45.3L96.2 44.7L82.1 32.7L54.9 31.9L79.8 34.9L92.1 46L79.7 47.4L65.9 52.8L55.1 60.4L31.9 54.2L21.3 71.4L23 86.4L17.1 100.2L18.7 114.6L9.1 107.3L7.4 61.8L10.2 41.2L14.7 38.1L14.6 31.3L26.2 16.8L41.7 8.2L46.9 8.6L50 4L68.8 1.2L89.2 3.5L94 7.5L96.2 5.6L117.2 12.3L131 28.8L133.4 36.3L136 39.8L141.1 58L142.1 87L138.4 109.2L132.7 116.9Z"/>
+                  <path id="Lnosed" d="M66.1 135.8L71.1 135.4L74.6 136.9L69 137.4L66.1 135.8Z"/>
+                  <path id="Rnosed" d="M83.8 137.7L87.6 135.7L92.3 136.6L88 138.3L83.8 137.7Z"/>
+                  <path id="Reye" d="M104.6 97.5L112.5 98.3L118.6 102.4L111.9 100.6L108.8 100.4L108 103.5L104.4 105L100.6 103.4L100 100.4L95.4 101.3L92.8 102.9L94.5 100.3L104.6 97.5Z"/>
+                  <path id="Leye" d="M54.1 97.3L61.7 99.7L63.9 102.4L61.1 100.6L58.4 100.3L57.9 102.9L53.8 104.4L49.4 102.8L48.9 99.9L45.6 99.8L40.8 101.6L45.2 98.6L54.1 97.3Z"/>
+                  <path id="mustch" d="M57.4 156.6L58.9 151.2L73.7 146.2L79.5 146.2L83.8 144.7L103.5 146.1L104 151.5L101 149.1L84.9 147.8L79.6 149.5L74.3 150L60.3 154.6L57.4 156.6Z"/>
+                  <path id="ReyeB" d="M88.6 97.3L92.4 90.3L115.7 90.7L122 96.1L115.9 95.3L98.2 95.9L88.6 97.3Z"/>
+                  <path id="LeyeB" d="M35 93.4L46.3 87L67 84.4L71.5 93.3L65.2 91.7L47.8 91.5L35 93.4Z"/>
+              </g>
+              <path id="outline" d="M9.1 107.3L7.4 61.8L10.2 41.2L14.7 38.1L14.6 31.3L26.2 16.8L41.7 8.2L46.9 8.6L50 4L68.8 1.2L89.2 3.5L94 7.5L96.2 5.6L117.2 12.3L131 28.8L133.4 36.3L136 39.8L141.1 58L142.1 87L138.4 109.2L143.9 109.4L147.5 124L140.7 144L135.4 153.5L129.3 154.1L120.7 171.8L109.1 187.2L91.1 199.9L68.4 201.2L51 189.9L30.2 167.5L22.9 151.7L16.9 152.1L8.4 144.3L1.5 124.3L4.1 109.8L9.1 107.3Z" stroke="black" strokeOpacity="0.9" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </section>
       </section>
     </main>
   )
